935a6bc5ab02ac4f67ec56433bcb6f1cd8a5aba8
pawelataman <pawel.atmn@gmail.com>
2025-03-25 16:33:57 +0100

    fix: import error

--- tab-share-client/src/app/core/services/base-http.service.ts
+++ tab-share-client/src/app/core/services/base-http.service.ts
@@ -1,31 +1,22 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { CoreFacade } from '../state/core.facade';
-import { catchError, defer, finalize, Observable, of } from 'rxjs';
-import { ToastService } from '../features/toaster/services/toast.service';
-import { ApiErrorMessageCode, ApiErrorMessageDictionary } from '../state/core.consts';
+import { defer, finalize, Observable } from 'rxjs';
 
 export class BaseHttpService {
-  protected BASE_URL = 'http://localhost:3000';
+  protected BASE_URL = 'http://localhost:8080';
   protected httpClient = inject(HttpClient);
   protected coreFacade = inject(CoreFacade);
-  private toastService = inject(ToastService);
 
   protected handleRequest<T>(request: Observable<T>): Observable<T> {
     return defer(() => {
       this.coreFacade.setLoading(true);
-      return request.pipe(catchError(this.onError.bind(this)), finalize(this.onComplete.bind(this)));
+      return request.pipe(finalize(this.onComplete.bind(this)));
     });
   }
 
-  private onError<T>(err: T): Observable<T> {
-    if (isHttpErrorResponse(err)) {
-      const message: string | undefined = ApiErrorMessageDictionary[err.error as ApiErrorMessageCode];
-      if (message) {
-        this.toastService.pushToast({ message, type: 'error', id: Date.now().toString() });
-      }
-    }
-    return of(err);
+  private onError<T>(err: T): Observable<never> {
+    throw err;
   }
 
   private onComplete(): void {
--- tab-share-client/src/app/tab-import/state/import.state.ts
+++ tab-share-client/src/app/tab-import/state/import.state.ts
@@ -3,8 +3,9 @@ import { ImportState, ImportTabsRequest } from './import.models';
 import { ImportHttpService } from '../services/import-http.service';
 import { inject } from '@angular/core';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
-import { pipe, switchMap } from 'rxjs';
+import { catchError, concatMap, of, pipe, switchMap, tap } from 'rxjs';
 import { tapResponse } from '@ngrx/operators';
+import { ToastService } from '../../core/features/toaster/services/toast.service';
 
 const INITIAL_STATE: ImportState = {
   tabs: [],
@@ -17,17 +18,25 @@ export const ImportStore = signalStore(
   withState<ImportState>(INITIAL_STATE),
   withMethods(store => {
     const httpService = inject(ImportHttpService);
+    const toastService = inject(ToastService);
     return {
       importTabs: rxMethod<ImportTabsRequest>(
         pipe(
-          switchMap((request: ImportTabsRequest) => httpService.importTabs(request)),
-          tapResponse(
-            response => {
-              patchState(store, { tabs: response.tabs });
-            },
-            error => {
-              console.log(error);
-            }
+          switchMap((request: ImportTabsRequest) =>
+            httpService.importTabs(request).pipe(
+              tapResponse(
+                response => {
+                  patchState(store, { tabs: response.tabs });
+                },
+                error => {
+                  toastService.pushToast({
+                    message: 'Could not import tabs',
+                    id: Date.now().toString(),
+                    type: 'error',
+                  });
+                }
+              )
+            )
           )
         )
       ),
